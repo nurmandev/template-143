17f29bcf4274fbfe08c39758cb7f95a8b623227d
nurmandev <adebayour66265@gmail.com>
2024-11-19 09:06:54 +0100

    update

--- src/components/animations/TextCharsFromRightToLeft.tsx
+++ src/components/animations/TextCharsFromRightToLeft.tsx
@@ -1,15 +1,13 @@
-import { useCurrentFrame } from 'remotion'
-import { defaultSpring, interpolateSpring } from '../../lib/helpers'
-
-
+import { useCurrentFrame } from 'remotion';
+import { defaultSpring, interpolateSpring } from '../../lib/helpers';
 
 export const TextCharsFromRightToLeft = ({ text }: { text: string }) => {
-  const frame = useCurrentFrame()
+  const frame = useCurrentFrame();
   return (
     <>
       {text.split('').map((char, i) => {
-        const spring = defaultSpring({ frame, delay: 6 + i * 1.1, durationInFrames: 20 })
-        if (char === '\n') return <br key={i} />
+        const spring = defaultSpring({ frame, delay: 6 + i * 1.1, durationInFrames: 20 });
+        if (char === '\n') return <br key={i} />;
         return (
           <span
             key={i}
@@ -21,8 +19,8 @@ export const TextCharsFromRightToLeft = ({ text }: { text: string }) => {
           >
             {char === ' ' ? <>&nbsp;</> : char}
           </span>
-        )
+        );
       })}
     </>
-  )
-}
+  );
+};
